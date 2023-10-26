771d7837547f5ca32e07f811fd2cba87881f0122
DavidDee2023 <d.nkosi@lab.clicklearning.org>
2023-10-26 13:58:03 +0200

    first commit

---
+++ Meme-Generator-DWA17/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Navbar from './components/Navbar'
+import Form from './components/Form'
+import './App.css'
+
+function App() { 
+
+  return (
+    <>
+      <Navbar />
+      <Form />
+    </>
+  )
+}
+
+export default App
